1d76500603819133d4422bcd0d1f13815642d82d
xiaoz <xiaoz93@outlook.com>
2021-01-15 10:51:33 +0800

    add

---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#####		brook relay		初始化脚本
+
+#更新软件包
+apk update
+#进入root目录
+cd /root
+
+#安装timezone
+apk add -U tzdata
+#查看时区列表
+ls /usr/share/zoneinfo
+#拷贝需要的时区文件到localtime
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+#查看当前时间
+date
+
+#安装curl
+apk add curl
+
+#安装brook
+curl -L http://soft.xiaoz.org/linux/rinetd -o /usr/bin/rinetd
+chmod +x /usr/bin/rinetd
+chmod +x /root/run.sh
+
+#卸载curl
+apk del curl tzdata
+
+echo '--------------------------------------'
+echo 'rinetd installation success.'
+echo '--------------------------------------'
